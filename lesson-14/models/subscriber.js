796954094bd3281f79816a6e884fbd0f285c066c
이준혁 <103807152+Terry929@users.noreply.github.com>
2024-05-01 02:34:42 +0000

    수업중...

--- lesson-14/models/subscriber.js
+++ lesson-14/models/subscriber.js
@@ -1,10 +1,21 @@
 // ./models/subscriber.js
 "use strict";
 
+const { default: mongoose } = require("mongoose");
+
 /**
  * @TODO: Listing 14.3 (p. 206)
  * 스키마 정의
  */
+const Mongoose = require("mongoose"),
+    subscriberSchema = mongoose.Schema({
+        name: String,
+        email: String,
+        newsletter: Boolean
+    });
 
-
+module.exports = mongoose.model(
+    "Subscriber",
+    subscriberSchema
+);
 // Subscriber 모델 생성
